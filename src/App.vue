1f92fbca554b03513ed3cec91297d65a92af0e14
pds.bocinas <pablo@handerr.com>
2017-09-10 18:01:50 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="app">
+    Hola!
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data() {
+    return {}
+  },
+  components: {
+
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+
+  watch: {
+
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+h1,
+h2 {
+    font-weight: normal;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+}
+
+a {
+    color: #42b983;
+}
+</style>
